c5487919376217325f72d68330accc7771e155ad
Yoshihide Shirai <yoshihide.shirai@gmail.com>
2024-04-30 16:03:01 +0900

    refactor

--- src/game/scenes/Game.ts
+++ src/game/scenes/Game.ts
@@ -19,28 +19,24 @@ export class Game extends Scene {
 
 
     draw_floor() {
-        let floor_width = this.floor_width;
-        let floor_height = this.floor_height;
         this.background = this.add.image(512, 384, 'background');
         this.background.setAlpha(0);
-        for (let i = 0; i < floor_width; i++) {
-            for (let j = 0; j < floor_height + 1; j++) {
+        for (let i = 0; i < this.floor_width; i++) {
+            for (let j = 0; j < this.floor_height + 1; j++) {
                 this.background = this.add.image(this.tale_size * i, this.tale_size * j, 'backwall').setDisplaySize(this.tale_size, this.tale_size)
             }
         }
-        for (let j = 0; j < floor_height + 1; j++) {
+        for (let j = 0; j < this.floor_height + 1; j++) {
             this.background = this.add.image(0, 0 + j * this.tale_size, 'sidewall').setDisplaySize(this.tale_size, this.tale_size)
-            this.background = this.add.image((floor_width - 1) * this.tale_size, 0 + j * this.tale_size, 'sidewall').setDisplaySize(this.tale_size, this.tale_size)
+            this.background = this.add.image((this.floor_width - 1) * this.tale_size, 0 + j * this.tale_size, 'sidewall').setDisplaySize(this.tale_size, this.tale_size)
         }
-        for (let i = 0; i < floor_width; i++) {
+        for (let i = 0; i < this.floor_width; i++) {
             this.background = this.add.image(0 + i * this.tale_size, 0, 'sidewall').setDisplaySize(this.tale_size, this.tale_size)
-            this.background = this.add.image(0 + i * this.tale_size, floor_height * this.tale_size, 'needle').setDisplaySize(this.tale_size, this.tale_size)
+            this.background = this.add.image(0 + i * this.tale_size, this.floor_height * this.tale_size, 'needle').setDisplaySize(this.tale_size, this.tale_size)
         }
     }
 
     create() {
-        let floor_width = this.floor_width;
-
         this.draw_floor()
 
         this.camera = this.cameras.main;
@@ -49,7 +45,7 @@ export class Game extends Scene {
         EventBus.emit('current-scene-ready', this);
 
         this.cameras.main.zoom = 1;
-        this.cameras.main.centerOn(floor_width * this.tale_size / 2 - this.tale_size / 2, 13 * this.tale_size / 2 - this.tale_size / 2);
+        this.cameras.main.centerOn(this.floor_width * this.tale_size / 2 - this.tale_size / 2, 13 * this.tale_size / 2 - this.tale_size / 2);
 
         this.gameText = this.add.text(this.tale_size * 2, this.tale_size, 'POWER : 20', {
             fontFamily: 'Arial Black', fontSize: this.tale_size / 2, color: '#ffffff',
