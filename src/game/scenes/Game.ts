124ac23a1494ed00f6b3391c4deef1ee490fc022
Yoshihide Shirai <yoshihide.shirai@gmail.com>
2024-04-30 16:01:03 +0900

    resize

--- src/game/scenes/Game.ts
+++ src/game/scenes/Game.ts
@@ -8,31 +8,33 @@ export class Game extends Scene {
 
     floor_width: integer;
     floor_height: integer;
+    tale_size: integer;
 
     constructor() {
         super('Game');
         this.floor_width = 17;
         this.floor_height = 12;
+        this.tale_size = 64
     }
 
 
-    draw_floor(){
+    draw_floor() {
         let floor_width = this.floor_width;
         let floor_height = this.floor_height;
         this.background = this.add.image(512, 384, 'background');
         this.background.setAlpha(0);
         for (let i = 0; i < floor_width; i++) {
             for (let j = 0; j < floor_height + 1; j++) {
-                this.background = this.add.image(32 * i, 32 * j, 'backwall')
+                this.background = this.add.image(this.tale_size * i, this.tale_size * j, 'backwall').setDisplaySize(this.tale_size, this.tale_size)
             }
         }
         for (let j = 0; j < floor_height + 1; j++) {
-            this.background = this.add.image(0, 0 + j * 32, 'sidewall')
-            this.background = this.add.image((floor_width - 1) * 32, 0 + j * 32, 'sidewall')
+            this.background = this.add.image(0, 0 + j * this.tale_size, 'sidewall').setDisplaySize(this.tale_size, this.tale_size)
+            this.background = this.add.image((floor_width - 1) * this.tale_size, 0 + j * this.tale_size, 'sidewall').setDisplaySize(this.tale_size, this.tale_size)
         }
         for (let i = 0; i < floor_width; i++) {
-            this.background = this.add.image(0 + i * 32, 0, 'sidewall')
-            this.background = this.add.image(0 + i * 32, floor_height * 32, 'needle')
+            this.background = this.add.image(0 + i * this.tale_size, 0, 'sidewall').setDisplaySize(this.tale_size, this.tale_size)
+            this.background = this.add.image(0 + i * this.tale_size, floor_height * this.tale_size, 'needle').setDisplaySize(this.tale_size, this.tale_size)
         }
     }
 
@@ -46,14 +48,15 @@ export class Game extends Scene {
 
         EventBus.emit('current-scene-ready', this);
 
-        this.cameras.main.zoom = 2;
-        this.cameras.main.centerOn(floor_width * 32 / 2 - 16, 13 * 32 / 2 - 16);
+        this.cameras.main.zoom = 1;
+        this.cameras.main.centerOn(floor_width * this.tale_size / 2 - this.tale_size / 2, 13 * this.tale_size / 2 - this.tale_size / 2);
 
-        this.gameText = this.add.text(64, 32, 'POWER : 20', {
-            fontFamily: 'Arial Black', fontSize: 16, color: '#ffffff',
+        this.gameText = this.add.text(this.tale_size * 2, this.tale_size, 'POWER : 20', {
+            fontFamily: 'Arial Black', fontSize: this.tale_size / 2, color: '#ffffff',
             stroke: '#000000', strokeThickness: 4,
             align: 'center'
         }).setOrigin(0.5).setDepth(100);
+        this.gameText.setScrollFactor(0, 0);
     }
 
     changeScene() {
