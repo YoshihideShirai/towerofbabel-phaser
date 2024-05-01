73cc09d30e49291cd2e3e10032c39aa0f9955a5f
Yoshihide Shirai <yoshihide.shirai@gmail.com>
2024-05-01 11:50:21 +0900

    draw sprite

--- src/game/scenes/Game.ts
+++ src/game/scenes/Game.ts
@@ -2,70 +2,115 @@ import { EventBus } from '../EventBus';
 import { Scene } from 'phaser';
 import * as yaml from 'js-yaml';
 
+type FloorConfig = {
+    name: string,
+    height: integer,
+    indy: { x: integer, y: integer },
+    gates: { x: integer, y: integer }[],
+    ivys: { x: integer, y: integer }[],
+    floors: { x: integer, y: integer }[],
+    blocks: { x: integer, y: integer, d: string }[],
+}
+
+type TowerConfig = {
+    name: string,
+    floors: FloorConfig[],
+}
+
 export class Game extends Scene {
     camera: Phaser.Cameras.Scene2D.Camera;
     background: Phaser.GameObjects.Image;
     gameText: Phaser.GameObjects.Text;
 
-    floor_width: integer;
-    floor_height: integer;
-    tale_size: integer;
+    floorConfig: FloorConfig
+    floorWidth: integer;
+    floorHeight: integer;
+    taleSize: integer;
 
     player: Phaser.GameObjects.Sprite;
+    gates: Phaser.GameObjects.Sprite[];
+    ivys: Phaser.GameObjects.Sprite[];
+    floors: Phaser.GameObjects.Sprite[];
+    blocks: Phaser.GameObjects.Sprite[];
 
     constructor() {
         super('Game');
-        this.floor_width = 17;
-        this.floor_height = 12;
-        this.tale_size = 64
+        this.floorWidth = 17;
+        this.floorHeight = 12;
+        this.taleSize = 64;
+        this.gates = [];
+        this.ivys = [];
+        this.floors = [];
+        this.blocks = [];
     }
 
-    preload(){
-        this.load.text('towerconfig','towers/front.yml')
+    preload() {
+        this.load.text('tower.config', 'towers/front.yml')
     }
 
-    fetchFloorData(){
-        let text = this.cache.text.get('towerconfig');
-        let data = yaml.load(text);
-        console.log(text)
-        console.log(data)
+    fetchFloorData() {
+        let text = this.cache.text.get('tower.config');
+        let data = yaml.load(text) as TowerConfig;
+        this.floorConfig = data.floors[0];
+        this.floorHeight = this.floorConfig.height;
     }
 
-    drawFloor() {
+    drawBackwall() {
         this.background = this.add.image(512, 384, 'background')
             .setAlpha(0);
-        for (let i = 0; i < this.floor_width; i++) {
-            for (let j = 0; j < this.floor_height + 1; j++) {
-                this.add.image(this.tale_size * i, this.tale_size * j, 'backwall')
-                    .setDisplaySize(this.tale_size, this.tale_size)
+        for (let i = 0; i < this.floorWidth; i++) {
+            for (let j = 0; j < this.floorHeight + 1; j++) {
+                this.add.image(this.taleSize * i, this.taleSize * j, 'backwall')
+                    .setDisplaySize(this.taleSize, this.taleSize)
             }
         }
-        for (let j = 0; j < this.floor_height + 1; j++) {
-            this.add.image(0, 0 + j * this.tale_size, 'sidewall')
-                .setDisplaySize(this.tale_size, this.tale_size)
-            this.add.image((this.floor_width - 1) * this.tale_size, 0 + j * this.tale_size, 'sidewall')
-                .setDisplaySize(this.tale_size, this.tale_size)
+        for (let j = 0; j < this.floorHeight + 1; j++) {
+            this.add.image(0, 0 + j * this.taleSize, 'sidewall')
+                .setDisplaySize(this.taleSize, this.taleSize)
+            this.add.image((this.floorWidth - 1) * this.taleSize, 0 + j * this.taleSize, 'sidewall')
+                .setDisplaySize(this.taleSize, this.taleSize)
         }
-        for (let i = 0; i < this.floor_width; i++) {
-            this.add.image(0 + i * this.tale_size, 0, 'sidewall')
-                .setDisplaySize(this.tale_size, this.tale_size)
-            this.add.image(0 + i * this.tale_size, this.floor_height * this.tale_size, 'needle')
-                .setDisplaySize(this.tale_size, this.tale_size)
+        for (let i = 0; i < this.floorWidth; i++) {
+            this.add.image(0 + i * this.taleSize, 0, 'sidewall')
+                .setDisplaySize(this.taleSize, this.taleSize)
+            this.add.image(0 + i * this.taleSize, this.floorHeight * this.taleSize, 'needle')
+                .setDisplaySize(this.taleSize, this.taleSize)
         }
+    }
+
+    addSpriteFromConfigIdx(x: integer, y: integer, texture: string): Phaser.GameObjects.Sprite {
+        let idxs = { x: (2 + x) * this.taleSize / 2, y: (2 + y) * this.taleSize / 2 }
+        return this.add.sprite(idxs.x, idxs.y, texture).setDisplaySize(this.taleSize, this.taleSize);
+    }
+
+    drawSprite() {
+        this.floorConfig.ivys.forEach(ele => {
+            this.floors.push(this.addSpriteFromConfigIdx(ele.x, ele.y, 'ivy'))
+        });
+        this.floorConfig.gates.forEach(ele => {
+            this.floors.push(this.addSpriteFromConfigIdx(ele.x, ele.y, 'gate'))
+        });
+        this.floorConfig.floors.forEach(ele => {
+            this.floors.push(this.addSpriteFromConfigIdx(ele.x, ele.y, 'floor'))
+        });
+        this.floorConfig.blocks.forEach(ele => {
+            this.floors.push(this.addSpriteFromConfigIdx(ele.x, ele.y, 'block_' + ele.d))
+        });
+        this.player = this.addSpriteFromConfigIdx(this.floorConfig.indy.x, this.floorConfig.indy.y, "indy_start3")
     }
 
     create() {
         this.fetchFloorData()
-        this.drawFloor()
+        this.drawBackwall()
 
         this.camera = this.cameras.main;
         this.camera.setBackgroundColor(0x000000);
 
         this.cameras.main.zoom = 1;
-        this.cameras.main.centerOn(this.floor_width * this.tale_size / 2 - this.tale_size / 2, 13 * this.tale_size / 2 - this.tale_size / 2);
+        this.cameras.main.centerOn(this.floorWidth * this.taleSize / 2 - this.taleSize / 2, 13 * this.taleSize / 2 - this.taleSize / 2);
 
-        this.gameText = this.add.text(this.tale_size * 2, this.tale_size, 'POWER : 20', {
-            fontFamily: 'Arial Black', fontSize: this.tale_size / 2, color: '#ffffff',
+        this.gameText = this.add.text(this.taleSize * 2, this.taleSize, 'POWER : 20', {
+            fontFamily: 'Arial Black', fontSize: this.taleSize / 2, color: '#ffffff',
             stroke: '#000000', strokeThickness: 4,
             align: 'center'
         })
@@ -73,7 +118,7 @@ export class Game extends Scene {
             .setDepth(100)
             .setScrollFactor(0, 0);
 
-        this.player = this.add.sprite(240, 80, "indy_start3").setDisplaySize(this.tale_size, this.tale_size);
+        this.drawSprite()
         EventBus.emit('current-scene-ready', this);
     }
 
